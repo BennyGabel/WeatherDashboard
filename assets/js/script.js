79b872fd42bfe1693f31cafe4aa6a523c8105eb5
13 Miles <bennygabel@gmail.com>
2022-09-02 13:41:11 -0400

    1st attempt to access URL API with API access code - error 401

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-alert('Hi');
+var getOpenWeatherApi = function(repo) {
+    console.log(repo);
+
+    // // // var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc"; 
+/*   var apiUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}   */
+    keyApi = "e361c27c104db2a481a66d649df15118";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=43.000000&lon=-75.000000&&appid="+keyApi;
+
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+          response.json().then(function(data) {
+            //console.log(data);
+            displayIssues(data);
+          });
+        }
+        else {
+          alert("There was a problem with your request!");
+        }
+    })
+};
+
+getOpenWeatherApi()
