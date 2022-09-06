f6ac650271e8f952663e75aa8e3057adf8250417
13 Miles <bennygabel@gmail.com>
2022-09-05 20:33:45 -0400

    Add function to calculate/convert Unix timestamp to a time in Javascrip, set additional variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ searchButtonEl = document.getElementById("searchButton")
 
 var keyApi = "e361c27c104db2a481a66d649df15118";
 
+var aryToday = [];    // Today's date - weather
+var ary5Fore = [];    // 5 days Forecast - Weather
+
+
 var getOpenWeatherForecast = function(cSearch) {
   // cSearch receives data from  getOpenWeatherApi
   console.log("Second API Call");
@@ -23,8 +27,8 @@ var getOpenWeatherForecast = function(cSearch) {
     // request was successful
     if (resp_2.ok) {
       resp_2.json().then(function(data2) {
-        console.log("Displaying Data2");
-        console.log(data2);
+      console.log("Displaying Data2");
+      console.log(data2);
 
 
       });
@@ -51,7 +55,6 @@ var getOpenWeatherApi = function(event) {
     //Template literals
     var apiUrl1 = `https://api.openweathermap.org/geo/1.0/direct?q=${userEntry}&limit=5&appid=${keyApi}`
 
-
     fetch(apiUrl1).then(function(response) {
         // request was successful
         if (response.ok) {
@@ -69,28 +72,16 @@ var getOpenWeatherApi = function(event) {
 };
 
 
+function convDate(pnUnix) {
+  // Convert Unix timestamp to a time in JavaScript
+  let unix = pnUnix;   /// i.e. 1507473344
+  let retDate = new Date(unix*1000);
 
+  return retDate
+}
 
 
 // Event listener 
 searchFormEl.addEventListener("submit", getOpenWeatherApi) 
 
 
-
-
-
-
-
-
-// function getApi() {
-//     // Replace `octocat` with anyone else's GitHub username
-//     keyApi = "e361c27c104db2a481a66d649df15118";
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=43.000000&lon=-75.000000&&appid="+keyApi;
-//     // var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=43.000000&lon=-75.000000&appid="+keyApi;
-//     fetch(apiUrl)
-//       .then(function(response) {
-//         console.log(response);
-//         return response.json();
-//       });
-// }
-//  getApi();
